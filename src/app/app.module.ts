c842775f34579ee77f3ce45f9fdc82bb3f1f7710
Felipe Lamim <fclamim@gmail.com>
2018-06-17 12:45:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AjudaPage } from '../pages/ajuda/ajuda';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AjudaPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AjudaPage
   ],
   providers: [
     StatusBar,
